36d923e43432d55cd43e9c5fa82133dd93d32cdd
camillegripon <camille.gripon@oclock.school>
2025-09-17 23:49:10 +0200

    toujours test DDB

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const { Pool } = pkg;
-
+const.log('DATABASE_URL:', process.env.DATABASE_URL);
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
