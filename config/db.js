9b556c3a04fccdd45caf4a44bec65a91e0f78ad8
camillegripon <camille.gripon@oclock.school>
2025-09-17 23:59:43 +0200

    toujours test DDB

--- config/db.js
+++ config/db.js
@@ -1,14 +1,16 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
+
 import dotenv from 'dotenv';
+
 dotenv.config();
-
+const { Pool } = pkg;
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // Obligatoire pour Supabase
-  },
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD, 
+    port: process.env.DB_PORT,
 });
 
 export default pool;
