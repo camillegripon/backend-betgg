f5a8e564ba1e9f17df528b118f7c4981f0eeb1c9
camillegripon <camille.gripon@oclock.school>
2025-09-18 00:31:06 +0200

    toujours test DDB

--- routes/test.js
+++ routes/test.js
@@ -3,6 +3,6 @@ const router = Router();
 import { testDB, testDNS } from '../controllers/testController.js';
 
 router.get('/test-db', testDB);
-router.get('test-dns', testDNS);
+router.get('/test-dns', testDNS);
 
 export default router; // Export par défaut
